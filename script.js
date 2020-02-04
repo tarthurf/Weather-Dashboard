59ab0af65f6a194fbdddbc52da53df704680b9f8
Swedish <tarthurf@yahoo.com>
2020-02-03 22:00:03 -0800

    added api call and variables for UV index

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ const apiKey = "c27ba2815803926d4006fd773d06f781";
 
 let cityName = "San Diego";
 
+let lat;
+
+let lon;
+
+const weatherIconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+
+let weatherIconCode;
+
+// Variables for API query calls
 const currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=" + apiKey;
 
 const fiveDayForcastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=" + apiKey;
 
-const currentDate = moment().format("dddd[,] MMMM Do YYYY")
-console.log(currentDate)
+const uvIndexAPI = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
 
-let iconCode;
+// Moment for calender date
+const currentDate = moment().format("dddd[,] MMMM Do YYYY");
+console.log(currentDate);
+
 
 const weatherIconLink = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
 
@@ -22,5 +33,5 @@ function getAPI(api) {
     });
 }
 
-getAPI(currentWeatherAPI)
-getAPI(fiveDayForcastAPI)
+getAPI(currentWeatherAPI);
+getAPI(fiveDayForcastAPI);
